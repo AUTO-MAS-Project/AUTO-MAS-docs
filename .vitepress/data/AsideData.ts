5c403fd556c5ba0a106e82fb5e064da697de96e2
DLmaster361 <DLmaster_361@163.com>
2025-12-10 02:44:09 +0800

    feat(aside): 添加 Telegram 群组链接与图标支持
    新增 Telegram 群组链接及图标，使用 simple-icons 提供的 Telegram SVG 图标。
    同时更新 QQ 群图标为官方 SVG 格式，优化图标在亮色和暗色模式下的显示效果。

--- .vitepress/data/AsideData.ts
+++ .vitepress/data/AsideData.ts
@@ -3,8 +3,14 @@ import type { AsideItem } from "@theojs/lumen";
 export const Aside_Data: AsideItem[] = [
   {
     link: "https://qm.qq.com/q/hdV065dcbu",
-    image: "/resource/AUTO_MAA.png",
-    name: "用户交流群",
-    info2: "QQ群号：957750551",
+    image: "https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/tencentqq.svg",
+    name: "QQ 官方群",
+    info2: "群号：957750551",
+  },
+  {
+    link: "https://t.me/AUTO_MAS_top",
+    image: "https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/telegram.svg",
+    name: "Telegram 群",
+    info2: "@AUTO_MAS_top",
   },
 ];
